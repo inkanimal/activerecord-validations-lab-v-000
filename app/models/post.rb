8c6947a7281b184ab76cd58fdeb53a9e7d2c9801
geoffrey woodcock <theatomproject@gmail.com>
2018-10-21 20:45:33 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
+class ClickbaitValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /(Won't Believe|Secret|Top \d|Guess)/
+      record.errors[attribute] << ("Not a clickbait title")
+    end
+  end
+end
+
+
+
+
 class Post < ActiveRecord::Base
   
   validates :title, presence: true, clickbait: true
