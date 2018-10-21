2bf9ea1c69cff47e20aed6dcb2adfcf086d1ebce
geoffrey woodcock <theatomproject@gmail.com>
2018-10-21 20:39:43 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+  
+  validates :name, presence: true, uniqueness: true
+  validates :bio, length: { maximum: 500 }
+  validates :password, length: { in: 6..20 }
+  validates :registration_number, length: { is: 6 }
 end
