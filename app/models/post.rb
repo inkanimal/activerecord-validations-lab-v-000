24a351039ac7e935227f63feea363d405414010f
geoffrey woodcock <theatomproject@gmail.com>
2018-10-21 20:38:49 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   
   validates :title, presence: true
-  validates :content, length: { minimum: 500 }
-  validates :password, length: { in: 6..20 }
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
   validates :registration_number, length: { is: 6 }
 end
