16b2e2e6c34ac05d5b8797b2cc8b2d518282b4f7
geoffrey woodcock <theatomproject@gmail.com>
2018-10-21 20:46:47 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ActiveRecord::Base
   
   validates :name, presence: true, uniqueness: true
-  validates :phone_numbers, length: { is: 10 }, uniqueness: true
+  validates :phone_number, length: { is: 10 }, uniqueness: true
   
 end
